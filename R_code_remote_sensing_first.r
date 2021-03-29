ffea43eb24634a5e070a399f238eb03a94787ba9
ivanasciacca <80622661+ivanasciacca@users.noreply.github.com>
2021-03-29 23:51:11 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -6,17 +6,17 @@ setwd("C:/lab/")
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 p224r63_2011
 
-# plot the bands
+# Plot the bands
 plot(p224r63_2011)
 
-# colour change
+# Colour change
 cl <- colorRampPalette(c("black","grey","light grey")) (100) 
 plot(p224r63_2011, col=cl)
 
 
 ## DAY 2
 
- # colour change -> new
+ # Colour change -> new
 cl <- colorRampPalette(c("blue","green","grey","orange","magenta","yellow")) (100) 
 plot(p224r63_2011, col=cl)
 
@@ -35,13 +35,13 @@ dev.off()
 
 ## DAY 3
 
-# plot band 1 with a predefined color ramp palette
+# Plot band 1 with a predefined color ramp palette
 plot(p224r63_2011$B1_sre)
 cls <- colorRampPalette(c("red","pink","orange","purple")) (200)
 plot(p224r63_2011$B1_sre, col=cls)
 dev.off()
 
-# plot 1 row, 2 columns 
+# 1 row, 2 columns 
 plot(p224r63_2011$B1_sre)
 plot(p224r63_2011$B2_sre)    
 par(mfrow=c(1,2))
@@ -53,21 +53,21 @@ par(mfrow=c(2,1))
 plot(p224r63_2011$B1_sre)
 plot(p224r63_2011$B2_sre)
 
-# plot the first four bands of Landsat
+# Plot the first four bands of Landsat
 par(mfrow=c(4,1))
 plot(p224r63_2011$B1_sre)
 plot(p224r63_2011$B2_sre)
 plot(p224r63_2011$B3_sre)
 plot(p224r63_2011$B4_sre)
 
-# plot a quadrat of bands
+# Plot a quadrat of bands
 par(mfrow=c(2,2))
 plot(p224r63_2011$B1_sre)
 plot(p224r63_2011$B2_sre)
 plot(p224r63_2011$B3_sre)
 plot(p224r63_2011$B4_sre)
 
-# plot  a quadrat of bands and colour change
+# Plot a quadrat of bands and colour change
 par(mfrow=c(2,2))
 clb <- colorRampPalette(c("dark blue","blue","light blue")) (100)
 plot(p224r63_2011$B1_sre, col=clb)
@@ -81,7 +81,7 @@ plot(p224r63_2011$B4_sre, col=clnir)
 
 ## DAY 4
 
-# visualising data by RGB plotting
+# Visualizing data by RGB plotting
 
 # Bands of Landsat
 # B1: blue
@@ -91,3 +91,38 @@ plot(p224r63_2011$B4_sre, col=clnir)
 # B5: middle infrared
 # B6: thermal infrared
 # B7: middle infrared
+
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="lin")
+
+# Exercise: mount a 2x2 multiframe
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="lin")
+
+# My first pdf with R
+pdf("my_first_pdf_with_R.pdf")
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="lin")
+dev.off()
+
+# Now use stretch hist
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
+
+# Par natural colours, false colours, and false colours with histogram stretch
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
+
+#  install.packages("RStoolbox")
+install.packages("RStoolbox")
+library(RStoolbox)
